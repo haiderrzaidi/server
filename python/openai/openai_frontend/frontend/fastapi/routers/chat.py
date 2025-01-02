9aa0e49c725d9f0e080acac073081f6c4aa58026
haiderrzaidi <zaidihaider336@gmail.com>
2025-01-02 18:54:52 +0500

    fix: Max Token Added

--- python/openai/openai_frontend/frontend/fastapi/routers/chat.py
+++ python/openai/openai_frontend/frontend/fastapi/routers/chat.py
@@ -45,6 +45,8 @@ async def create_chat_completion(
         raise HTTPException(status_code=500, detail="No attached inference engine")
 
     try:
+        if request.max_tokens is None:
+            request.max_tokens = 4000  # Set your default value here
         response = await raw_request.app.engine.chat(request)
         if request.stream:
             return StreamingResponse(response, media_type="text/event-stream")
